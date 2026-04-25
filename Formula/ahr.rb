cfb71ccfb7a0ba670a0e4f6e956711c35e86b9c3
Carl Lin <114946043+Chen-Lim@users.noreply.github.com>
2026-04-25 23:39:23 +1000

    update for v0.3.0

--- Formula/ahr.rb
+++ Formula/ahr.rb
@@ -2,7 +2,7 @@ class Ahr < Formula
   desc "Apple Health Resonator: 高性能苹果健康数据解析工具"
   homepage "https://github.com/Chen-Lim/Apple-Health-Resonator-CLI/archive/refs/tags/v0.3.0.tar.gz"
 
-  url " https://github.com/Chen-Lim/Apple-Health-Resonator-CLI/archive/refs/tags/v0.3.0.tar.gz"
+  url "https://github.com/Chen-Lim/Apple-Health-Resonator-CLI/archive/refs/tags/v0.3.0.tar.gz"
   version "0.3.0"
 
   sha256 "2bd71907d02497ed8c2baae7c56b38558ef2fae7f00c17b3a0e697266d1d7b49"
